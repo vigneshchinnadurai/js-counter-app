92560bb04ed3626117d366f0a11c5706933ccab4
Vignesh Chinnadurai <donowglobal@gmail.com>
2024-03-10 20:51:12 +0530

    Random Gen App

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
 
-buttons.addEventListener("click", (event) => {
-  let currentValue = Number.parseInt(count.innerHTML);
-  if (event.target.classList.contains("add")) {
-    currentValue += 1;
-    count.innerHTML = currentValue;
-    setColor();
-  }
-  if (event.target.classList.contains("subtract")) {
-    currentValue -= 1;
-    count.innerHTML = currentValue;
-    setColor();
-  }
-  if (event.target.classList.contains("reset")) {
-    currentValue = 0;
-    count.innerHTML = currentValue;
-    setColor();
-  }
-});
-
-const setColor = () => {
-  if (count.innerHTML > 0) {
-    count.style.color = "yellow";
-  } else if (count.innerHTML < 0) {
-    count.style.color = "orangered";
-  } else {
-    count.style.color = "#fff";
-  }
+const generateNumber = () => {
+  // Generate number between 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.textContent = randomNumber;
 };
+
+btn.addEventListener("click", generateNumber);
